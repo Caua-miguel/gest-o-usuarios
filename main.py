a05680c25161e3286f26076c8e720acc4ae0e0b0
Caua-miguel <caua.sudol@outlook.com>
2025-01-01 21:51:26 -0300

    feat: swap database

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from flask import Flask
-from routes.home import home_route
-from routes.client import client_route
+from configuration import configure_all
 
 # inicialização
 app = Flask(__name__)
 
-app.register_blueprint(home_route)
-app.register_blueprint(client_route, url_prefix='/clientes')
+configure_all(app)
 
 # execução
 app.run(debug=True)
